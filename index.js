a329611b6360a1cc26deaf625f2da5cd1a9bb60d
engr-faysal <engr.mdfaysal@gmail.com>
2025-06-29 16:15:35 +0600

    Object instance

--- index.js
+++ index.js
@@ -12,4 +12,13 @@
         console.log(person.getName())
 
 
-
+/* Object instance*/
+        let daughter= new Object();
+        daughter.first_name="Minha",
+        daughter.last_name="Ayat",
+        daughter.age=0,
+        daughter.city="Dhaka"
+        daughter.getName= function(){
+            return `Daughter's Name ${daughter.first_name} ${daughter.last_name}`
+        }
+        console.log(daughter.getName())
